6c9dcd113257f0af0919b80f3b297b13eab04e0f
ayush <154315834+ayushx07-web@users.noreply.github.com>
2026-08-14 16:20:59 +0530

    Time: 11 ms (83.29%) | Memory: 59.5 MB (63.13%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -4,22 +4,18 @@ class Solution {
     }
 
     private int atmost(int[] nums, int k){
-        int left =0;
-        int sum = 0;
+        int left = 0;
         int count = 0;
-        
-        if(k<0) return 0;
+        int sum = 0;
 
         for(int right=0;right<nums.length;right++){
             sum+= nums[right]%2;
-
             while(sum>k){
                 sum-= nums[left]%2;
                 left++;
-
             }
             count+= right-left+1;
         }
-           return count;
+        return count;
 }
 }
